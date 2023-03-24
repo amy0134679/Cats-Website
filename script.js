c5f02a5bcd3a64609b660006b1cbdb242a0f9aae
Amy <amy0134679@gmail.com>
2023-03-24 13:17:44 -0400

    Improved Navbar

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 script.js
 
+function change(evt, content) {
+    var i, content, navbar;
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+      content[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
