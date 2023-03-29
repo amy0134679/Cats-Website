6e269bcb05b4241de7a6183e60278c25ac5bcd44
Amy <amy0134679@gmail.com>
2023-03-29 00:41:27 -0400

    Created image carousel

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-script.js
-
-function change(evt, content) {
-    var i, content, navbar;
-    content = document.getElementsByClassName("content");
-    for (i = 0; i < content.length; i++) {
-      content[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
